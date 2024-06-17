0357697dfc660b44b63cade6e4a6baee9a6aebf8
Felipe Yáñez <3996991+felipeyanez@users.noreply.github.com>
2024-06-17 21:27:27 +0100

    added features extraction ipfx helper

---
+++ notebooks/features_extraction/ipfx/bin/pipeline_from_specimen_id.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+OUTPUT_DIR=/local1/ephys/tsts
+SPECIMEN_ID=$1
+
+CELL_NAME=$SPECIMEN_ID
+CELL_DIR=$OUTPUT_DIR/$CELL_NAME
+INPUT_JSON=$CELL_DIR/pipeline_input.json
+OUTPUT_JSON=$CELL_DIR/pipeline_output.json
+LOG_FILE=$CELL_DIR/log.txt
+
+echo $CELL_DIR
+
+mkdir -p $CELL_DIR
+
+python generate_pipeline_input.py --specimen_id $SPECIMEN_ID --cell_dir $CELL_DIR
+python run_pipeline.py --input_json $INPUT_JSON --output_json $OUTPUT_JSON --log_level DEBUG |& tee $LOG_FILE
